3bee9060626f35d8451917fd385fbd39e051e387
梁典典 <413153189@qq.com>
2022-12-09 14:56:46 +0800

    新增`FBuildWidget`小部件

--- lib/mixin/widget.dart
+++ lib/mixin/widget.dart
@@ -1,5 +1,27 @@
 part of dd_js_util;
-mixin WidgetBodyBuildMixin  {
+
+mixin WidgetBodyBuildMixin {
   ///构建主显示的小部件
-  Widget buildCoreBodyWidget(BuildContext context,[WidgetRef? ref]);
+  Widget buildCoreBodyWidget(BuildContext context, [WidgetRef? ref]);
+}
+
+mixin SimplePageUtilMixin<T extends StatefulWidget> on State<T> {
+
+  bool _widgetLoading = false;
+  set widgetLoading(bool v) {
+    noticeUiRefresh(() {
+      _widgetLoading = v;
+    });
+  }
+  bool get widgetLoading => _widgetLoading;
+
+  void noticeUiRefresh(VoidCallback call) {
+    call.call();
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+
+
 }
